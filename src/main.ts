fb452e95783deb4792931817394e2a641d0157a8
llluper <mitchelljstanford@gmail.com>
2019-05-13 16:36:54 +1000

    Add vue-resource and vue-ctk-date-time-picker dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
+import VueResource from 'vue-resource';
 
+Vue.use(VueResource);
+Vue.component('VueCtkDateTimePicker', VueCtkDateTimePicker);
 Vue.config.productionTip = false;
 
 new Vue({
