fcef20ac34a8edf0963fd3adb6036780bd5eb296
llluper <mitchelljstanford@gmail.com>
2019-05-13 16:42:41 +1000

    Update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Welcome to the Carpark Rate Calculator.</h1>
+    <h3>Enter an Entry AND an Exit, date and time, to calculate your rate!</h3>
+    <h3>Developed by <a href="https://github.com/llluper" target="_blank">Mitchell Stanford (@llluper)</a></h3>
   </div>
 </template>
