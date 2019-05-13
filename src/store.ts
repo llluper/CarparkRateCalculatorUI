0575b28e27a85e6b990ff93c95b214c3a40ee4e7
llluper <mitchelljstanford@gmail.com>
2019-05-13 16:37:51 +1000

    Create store state, actions and mutations

--- src/store.ts
+++ src/store.ts
@@ -3,14 +3,32 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+interface StateType {
+  entryDate: string;
+  exitDate: string;
+  result: any;
+}
+
 export default new Vuex.Store({
   state: {
-
+    entryDate: '',
+    exitDate: '',
+    result: { name: '', cost: 0.00 },
   },
   mutations: {
-
+    saveEntryDate(state: StateType, date: string) {
+      state.entryDate = date;
+    },
+    saveExitDate(state: StateType, date: string) {
+      state.exitDate = date;
+    },
+    saveResult(state: StateType, result: any) {
+      state.result = result;
+    },
   },
   actions: {
-
+    saveEntryDate: ({ commit }, payload: string) => commit('saveEntryDate', payload),
+    saveExitDate: ({ commit }, payload: string) => commit('saveExitDate', payload),
+    saveResult: ({ commit }, payload: any) => commit('saveResult', payload),
   },
 });
